546e2d55548fcc1dcd76f836dc6d7377c38b4601
DESKTOP-8D661U9\pinecone <opineconeo@gmail.com>
2018-10-05 13:27:06 +0800

    將繪圖相關從test.py轉移到PaperRecordTools.py中，合併參數。

--- source/PaperRecordTools.py
+++ source/PaperRecordTools.py
@@ -1,6 +1,41 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as pyplot
+from SolarPaperRecord import SolarPaperRecord
+
+# 繪製一般輸出結果
+def show_draw_data(p, show_image = True, write_image = False):
+  if issubclass(type(p), SolarPaperRecord):
+    show_draw_solar_data(p, show_image, write_image)
+  else:
+    show_draw_normal_data(p, show_image, write_image)
+
+# 繪製除日照圖以外輸出結果
+def show_draw_normal_data(p, show_image = True, write_image = False):
+  image = p.src_image.copy()
+  draw_data(p, image)
+  draw_value_region(p, image)
+  
+  if write_image:
+    out_name = "../out/" + p.name + "_out.png"
+    cv2.imwrite(out_name, image)
+    
+  if show_image:
+    image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
+    cv2.imshow(p.name, image)
+    
+# 繪製日照計圖輸出結果
+def show_draw_solar_data(p, show_image = True, write_image = False):
+  image = p.src_image.copy()
+  draw_solar_data(p, image) 
+  
+  if write_image:
+    out_name = "../out/" + p.name + "_out.png"
+    cv2.imwrite(out_name, image)
+  
+  if show_image:
+    image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
+    cv2.imshow(p.name, image)
 
 # 繪製圖形比對的match結果圖
 def draw_match(paperRecord, name, show_image = True, write_image = False):
@@ -50,4 +85,81 @@ def write_mask_text(paperRecord, name, show_image = True, write_image = False):
   if show_image:
     mask_text = cv2.resize(mask_text, (0,0), fx=0.3, fy=0.3)
     cv2.imshow(name + '_text', mask_text)
+
+#
+# 以下為private functions
+#
+
+# 畫出該圖的數值接受區域
+def draw_value_region(p, image):
+  pts = []
+  pts.append(p.image_coordiante([p.value_region[0], p.value_region[1]])[0:2])
+  pts.append(p.image_coordiante([p.value_region[0], p.value_region[3]])[0:2])
+  pts.append(p.image_coordiante([p.value_region[2], p.value_region[3]])[0:2])
+  pts.append(p.image_coordiante([p.value_region[2], p.value_region[1]])[0:2])
+  cv2.polylines(image,[np.int32(pts)],True,255,1, cv2.LINE_AA)
+  return image
+
+# 在圖上 x,y 標出數值
+def draw_value(p, image, x, y):
+  i = p.image_coordiante([y, x])
+  s = "%.2f" % y
+  cv2.putText(image, s, (int(i[0]) + 10, int(i[1])), cv2.FONT_HERSHEY_SIMPLEX, 1.,(255,0,0), 1, cv2.LINE_AA)
   
+# 內插計算值
+def interpolation_data(c, y):
+  for i in range(len(c)):
+    if c[i][1] >= y:
+      if i <= 0: return None
+      y0 = c[i-1][1]
+      y1 = c[i][1]
+      x0 = c[i-1][0]
+      x1 = c[i][0]
+      return ((y1-y)*x0 + (y-y0)*x1)/(y1-y0)
+  return None
+  
+# 畫出該圖的數值點
+def draw_data(p, image):
+  c = p.max_value_line()
+  for v in c:
+    i = p.image_coordiante(v)
+    cv2.circle(image, (int(i[0]), int(i[1])), 1, (0, 255, 0))
+    # print("%02d:%02d %f" % ((v[1]//60) + 8, v[1]%60, v[0]))
+    
+  for i in range(p.value_region[1], p.value_region[3], 60):
+    x = interpolation_data(c, i)
+    if x is None: continue
+    draw_value(p, image, i, x) 
+    
+  return image
+
+# 繪製日照計的數值點
+def draw_solar_data(p, image):
+
+  # 計算出太陽燒灼點
+  solar_data = p.solar_data()
+  
+  # 繪製出燒灼點的兩端點
+  for i in range(len(solar_data)):
+    v = solar_data[i]
+    if v is not None:
+      n = p.image_coordiante((v[0], i))
+      cv2.circle(image, (int(n[0]), int(n[1])), 3, (0, 0, 255))
+      n = p.image_coordiante((v[1], i))
+      cv2.circle(image, (int(n[0]), int(n[1])), 3, (0, 0, 255))
+      
+  # 分成12個小時個別統計資料數量(當作日照時數)
+  hour_sum = np.zeros(12, np.float32)
+  for i in range(len(solar_data)):
+    v = solar_data[i]
+    if v is not None:
+      hour_sum[i // 60] += 1     
+  hour_sum /= 60.
+  
+  # 每小時顯示統計日照時數
+  for i in range(12):
+    n = p.image_coordiante((p.image_r + 100, i * 60. + 30.))
+    s = "%.02f" % hour_sum[i]
+    cv2.putText(image, s, (int(n[0]), int(n[1])), cv2.FONT_HERSHEY_SIMPLEX, 1.,(0,0,255), 1, cv2.LINE_AA)
+
+  return image
--- source/test.py
+++ source/test.py
@@ -10,98 +10,6 @@ from SolarLongPaperRecord import SolarLongPaperRecord
 from SolarMediumPaperRecord import SolarMediumPaperRecord
 from OldRainPaperRecord import OldRainPaperRecord
 from PaperRecordTools import *
-
-# 畫出該圖的數值接受區域
-def draw_value_region(p, image):
-  pts = []
-  pts.append(p.image_coordiante([p.value_region[0], p.value_region[1]])[0:2])
-  pts.append(p.image_coordiante([p.value_region[0], p.value_region[3]])[0:2])
-  pts.append(p.image_coordiante([p.value_region[2], p.value_region[3]])[0:2])
-  pts.append(p.image_coordiante([p.value_region[2], p.value_region[1]])[0:2])
-  cv2.polylines(image,[np.int32(pts)],True,255,1, cv2.LINE_AA)
-  return image
-
-# 在圖上 x,y 標出數值
-def draw_value(p, image, x, y):
-  i = p.image_coordiante([y, x])
-  s = "%.2f" % y
-  cv2.putText(image, s, (int(i[0]) + 10, int(i[1])), cv2.FONT_HERSHEY_SIMPLEX, 1.,(255,0,0), 1, cv2.LINE_AA)
-  
-# 內插計算值
-def interpolation_data(c, y):
-  for i in range(len(c)):
-    if c[i][1] >= y:
-      if i <= 0: return None
-      y0 = c[i-1][1]
-      y1 = c[i][1]
-      x0 = c[i-1][0]
-      x1 = c[i][0]
-      return ((y1-y)*x0 + (y-y0)*x1)/(y1-y0)
-  return None
-  
-# 畫出該圖的數值點
-def draw_data(p, image):
-  c = p.max_value_line()
-  for v in c:
-    i = p.image_coordiante(v)
-    cv2.circle(image, (int(i[0]), int(i[1])), 1, (0, 255, 0))
-    # print("%02d:%02d %f" % ((v[1]//60) + 8, v[1]%60, v[0]))
-    
-  for i in range(p.value_region[1], p.value_region[3], 60):
-    x = interpolation_data(c, i)
-    if x is None: continue
-    draw_value(p, image, i, x) 
-    
-  return image
-
-# 繪製日照計的數值點
-def draw_solar_data(p, image):
-
-  # 計算出太陽燒灼點
-  solar_data = p.solar_data()
-  
-  # 繪製出燒灼點的兩端點
-  for i in range(len(solar_data)):
-    v = solar_data[i]
-    if v is not None:
-      n = p.image_coordiante((v[0], i))
-      cv2.circle(image, (int(n[0]), int(n[1])), 3, (0, 0, 255))
-      n = p.image_coordiante((v[1], i))
-      cv2.circle(image, (int(n[0]), int(n[1])), 3, (0, 0, 255))
-      
-  # 分成12個小時個別統計資料數量(當作日照時數)
-  hour_sum = np.zeros(12, np.float32)
-  for i in range(len(solar_data)):
-    v = solar_data[i]
-    if v is not None:
-      hour_sum[i // 60] += 1     
-  hour_sum /= 60.
-  
-  # 每小時顯示統計日照時數
-  for i in range(12):
-    n = p.image_coordiante((p.image_r + 100, i * 60. + 30.))
-    s = "%.02f" % hour_sum[i]
-    cv2.putText(image, s, (int(n[0]), int(n[1])), cv2.FONT_HERSHEY_SIMPLEX, 1.,(0,0,255), 1, cv2.LINE_AA)
-
-  return image
-  
-def show_draw_data(p, name):
-  image = p.src_image.copy()
-  draw_data(p, image)
-  draw_value_region(p, image)
-  
-  out_name = "../out/" + name + "_out.png"
-  #cv2.imwrite(out_name, image)
-  image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
-  cv2.imshow(name, image)
-    
-def show_draw_solar_data(p, name):
-  image = p.src_image.copy()
-  draw_solar_data(p, image) 
-  out_name = "../out/" + name + "_out.png"
-  #cv2.imwrite(out_name, image)
-  image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
-  cv2.imshow(name, image)
     
 def test_rain():
   files = [\
@@ -111,7 +19,7 @@ def test_rain():
 
   for file_name in files:
     r = RainPaperRecord(file_name)
-    show_draw_data(r, r.name)
+    show_draw_data(r)
 
   cv2.waitKey(0)
   
@@ -125,7 +33,7 @@ def test_old_rain():
     
   for file_name in files:
     r = OldRainPaperRecord(file_name)
-    show_draw_data(r, r.name)
+    show_draw_data(r)
 
   cv2.waitKey(0)
   
@@ -138,7 +46,7 @@ def test_pressure():
     
   for file_name in files:
     r = PressurePaperRecord(file_name)
-    show_draw_data(r, r.name)
+    show_draw_data(r)
 
   cv2.waitKey(0)
   
@@ -152,7 +60,7 @@ def test_temperature():
     
   for file_name in files:
     r = TemperaturePaperRecord(file_name)
-    show_draw_data(r, r.name)
+    show_draw_data(r)
 
   cv2.waitKey(0)
   
@@ -166,7 +74,7 @@ def test_humidity():
     
   for file in files:
     r = TemperaturePaperRecord(file_name = file[0], name = file[1])
-    show_draw_data(r, r.name)
+    show_draw_data(r)
 
   cv2.waitKey(0)
   
@@ -178,7 +86,7 @@ def test_solar_short():
     
   for file_name in files:
     r = SolarShortPaperRecord(file_name)
-    show_draw_solar_data(r, r.name)
+    show_draw_solar_data(r)
 
   cv2.waitKey(0)
   
@@ -189,7 +97,7 @@ def test_solar_long():
     
   for file_name in files:
     r = SolarLongPaperRecord(file_name)
-    show_draw_solar_data(r, r.name)
+    show_draw_data(r)
 
   cv2.waitKey(0)
   
@@ -200,7 +108,7 @@ def test_solar_medium():
     
   for file_name in files:
     r = SolarMediumPaperRecord(file_name)
-    show_draw_solar_data(r, r.name)
+    show_draw_solar_data(r)
 
   cv2.waitKey(0)
  
@@ -235,31 +143,3 @@ test_solar_long()
 #write_mask_text(SolarShortPaperRecord(cv2.imread('../sample/solar_short_01.jpg')), 'solar_short_01', write_image = True)
 #cv2.waitKey(0)
 
-#p = OldRainPaperRecord(cv2.imread('../sample/old_rain_03.jpg'))
-
-#text_image = p.mask_text()
-#text_image = cv2.resize(text_image, (0,0), fx=0.3, fy=0.3)
-#cv2.imshow('text_image', text_image)
-
-#grid_image = p.mask_grid()
-#grid_image = cv2.resize(grid_image, (0,0), fx=0.3, fy=0.3)
-#cv2.imshow('grid_image', grid_image)
- 
-#cv2.waitKey(0)
- 
-'''
-img = cv2.imread('../sample/rain_01.png')
-#p = PressurePaperRecord(img)
-p = RainPaperRecord(img)
-
-img2 = img.copy()
-c = p.max_value_line()
-for v in c:
-  i = p.image_coordiante(v)
-  cv2.circle(img2, (int(i[0]), int(i[1])), 1, (0, 255, 0))
-#img2 = p.match_image()
-#img2 = p.mask_text()
-img2 = cv2.resize(img2, (0,0), fx=0.3, fy=0.3)
-cv2.imshow('text_image', img2)
-cv2.waitKey(0)
-'''
